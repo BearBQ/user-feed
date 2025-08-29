7237dc2daf399d730af21c89cb70bbf613987b0a
BearBQ <d.a.demkin@gmail.com>
2025-08-29 20:31:04 +0500

    new_tree

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module 8_final
 
 go 1.24.6
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("No .env file found")
+	}
+}
